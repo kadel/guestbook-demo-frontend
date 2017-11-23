7b8bca72a42652b2e039d818a6bae2d7453f0e05
Tomas Kral <tkral@redhat.com>
2017-11-23 17:45:49 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule, MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { MatInkBar } from '@angular/material/tabs/typings/ink-bar';
 
 
 @NgModule({
@@ -10,7 +11,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
